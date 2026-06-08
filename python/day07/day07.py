4c6b9b234ee9e87a2f96d04a4a6cac7d94d21bc8
Darshan Giri <darshangiri785@gmail.com>
2026-06-08 09:04:38 -0500

    Day 07- Easy Exercises

--- python/day07/day07.py
+++ python/day07/day07.py
@@ -1,5 +1,6 @@
 #Passing a Arbitrary Number of Arguments
 # To pass a random number of arguments, use asterik before the definition parameter
+#Single asterik makes a tuple
 def make_pizza(*toppings):
     print(toppings)
 
@@ -15,3 +16,37 @@ def make_pizza(size, *toppings):
 
 make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
 
+#Using arbitrary keyword arguments
+#The doble asterik helps create a dictionary with key pair values
+def build_profile(first, last, **user_info):
+    profile={}
+    profile['first name']=first
+    profile['last name']=last
+    for key,value in user_info.items():
+        profile[key]=value
+    return profile
+
+user_profile=build_profile('albert','einstein',location='princeton',field='physics')
+
+print(user_profile)
+
+#Importing an entire module
+""" Importting a module. A module means a file ending in .py
+    You can call them by the word inport for example if the file is pizza.py
+    then you can import them by import pizza
+    after that you need '.' to access the function in the file. For example if the
+    for example piiza.make_pizza(arguments), you can also import only the one that you need
+    for example from pizza omport make_pizza, then you can directly use the function my itself.
+    
+    You can also give alias
+        eg:- from pizza import make_pizza as mp
+        
+    If you want to import all the functions, you can
+    from pizza import *
+    
+    Things about styling a function
+    Use names that help the user get the hold of the idea of a function parameter
+    If you have a default value , there should be no spaces either side of the equal sign
+    Comment should be right after the function definition.
+    every line should be less than 79 characters, if the function has more parameters, press enter to change line
+    """
