2e5603c1af91863609b1a090611f0cdf584d29a8
Darshan Giri <darshangiri785@gmail.com>
2026-06-11 20:03:05 -0500

    Day 09- manipulating classes

---
+++ python/day09/day09.py
@@ -0,0 +1,53 @@
+#Reviewing yesterday a little bit.
+# Setting a Default Value for an Attribute
+
+class Car():
+    def __init__(self, make, model, year):
+        self.make=make
+        self.model=model
+        self.year=year
+        self.odometer_reading=0
+    def get_descriptive_name(self):
+        print(str(self.year)+' ' +self.make + " "+ self.model )
+
+    def read_odometer(self):
+        print("This car has "+str(self.odometer_reading) + " miles on it.")
+    
+    def update_odometer(self, mileage):
+        if mileage>= self.odometer_reading:    
+            self.odometer_reading=mileage
+        else:
+            print("you can't roll back an odometer!")
+    
+    def increment_odometer(self, miles):
+        self.odometer_reading+=miles
+        
+
+my_new_car=Car('audi', 'a4', 2016)
+my_new_car.get_descriptive_name()
+my_new_car.read_odometer()
+my_new_car.odometer_reading=2300
+my_new_car.read_odometer()
+my_new_car.update_odometer(37)
+my_new_car.read_odometer()
+my_new_car.increment_odometer(100)
+
+
+#Inheritance
+""" If you are writing a class which is a specialized version of another class you wrote, you can use 
+    inheritance. It takes on all attributes of the first class hence forming parent and child class."""
+
+# The init method for a child class
+
+class ElectricCar(Car):
+    def __init__(self, make, model, year):
+        # The super function is a special function which helps make the connection between the parent and child class
+# Defining attributes and methods for the child class
+        super().__init__(make, model, year)
+        self.battery_size=70
+    def describe_battery(self):
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+my_tesla=ElectricCar('tesla', 'model s', 2016)
+my_tesla.get_descriptive_name()
+my_tesla.describe_battery()
