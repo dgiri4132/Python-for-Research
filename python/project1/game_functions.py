cafc2badce87d09258c5daf1e976a58feef44707
Darshan Giri <darshangiri785@gmail.com>
2026-07-17 19:32:42 -0500

    Finished project 1

--- python/project1/game_functions.py
+++ python/project1/game_functions.py
@@ -29,12 +29,16 @@ def update_screen(ai_settings, screen, ship, aliens,bullets):
     that seems to be right as well. Also we need to assign what we need to given that the certain key is pressed as well"""
 
 def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
-    stats.ship_left-=1
-    aliens.empty()
-    bullets.empty()
-    create_fleet(ai_settings, screen, ship, aliens)
-    ship.center_ship()
-    sleep(0.5)
+    if stats.ships_left>0:
+        stats.ship_left-=1
+        aliens.empty()
+        bullets.empty()
+        create_fleet(ai_settings, screen, ship, aliens)
+        ship.center_ship()
+        sleep(0.5)
+    else:
+        stats.game_active = False
+
 """The check events function will grow as the project grows so it is better if we separate them for key up and down as well"""
 def fire_bullet(ai_settings, screen, ship, bullets):
     """Fire a bullet if limit not reached yet."""
@@ -127,11 +131,20 @@ def check_fleet_edges(ai_settings, aliens):
         if alien.check_edges():
             change_fleet_direction(ai_settings, aliens)
             break
+def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
+    screen_rect = screen.get_rect()
+    for alien in aliens.sprites():
+        if alien.rect.bottom>=screen_rect.bottom:
+            ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+            break
 
 def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     check_fleet_edges(ai_settings, aliens)
     aliens.update()
+    check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
 
     if pygame.sprite.spritecollideany(ship, aliens):
         ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
 
+
+    
