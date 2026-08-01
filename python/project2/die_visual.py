0a21e1abb3968fcee27c8dd3cb07aa2a60a1fcff
Darshan Giri <darshangiri785@gmail.com>
2026-07-31 23:24:20 -0500

    Did refactoring

--- python/project2/die_visual.py
+++ python/project2/die_visual.py
@@ -11,11 +11,14 @@ for roll_num in range(100):
 
 frequencies = []
 max_result = die_1.num_sides + die_2.num_sides
+x_labels=[]
 for value in range(2, max_result+1 +1):
+    x_labels.append(str(value))
     frequency = results.count(value)
     frequencies.append(frequency)
 
 hist = pygal.Bar()
+hist.x_labels = x_labels
 hist.title = "Results of rolling one D6 100 times."
 hist.x_labels = ['2','3','4','5','6','7','8','9','10','11','12']
 hist.x_title = "Result"
