31bb6c9a3fd66bcdb9229c18058343a6c4060f7e
Darshan Giri <darshangiri785@gmail.com>
2026-08-03 23:16:26 -0500

    I did the datetime extraction today along with plotting two curves and shading the area in between, also put in an error handling mechanism

--- python/chapter16/high_lows.py
+++ python/chapter16/high_lows.py
@@ -1,21 +1,46 @@
 import csv
 from matplotlib import pyplot as plt
-filename = 'python/chapter16/sitka_weather_07-2014.csv'
+from datetime import datetime
+filename = 'python/chapter16/sitka_weather_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    highs = []
+    dates,highs, lows = [], [], []
     for row in reader:
+        current_date = datetime.strptime(row[0], "%Y-%m-%d")
+        dates.append(current_date)
         high = int(row[1])
         highs.append(high)
+        low = int(row[3])
+        lows.append(low)
 
 fig = plt.figure(dpi = 128, figsize = (10,6))
-plt.plot(highs, c = 'red')
+plt.plot(dates, highs, c = 'red', alpha = 0.5)
+plt.plot(dates, lows, c = 'blue', alpha = 0.5)
+plt.fill_between(dates, highs, lows, facecolor = 'blue', alpha = 0.1)
 
-plt.title("Daily high temperatures, Huly 2014", fontsize = 24)
+plt.title("Daily high  and low temperatures - 2014", fontsize = 24)
 plt.xlabel('', fontsize = 16)
+fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize = 16)
 plt.tick_params(axis = 'both', which = 'major', labelsize = 16)
 
 plt.show()
 
+
+
+"""
+To remember arguments for the datetime below is the table
+
+%A      Weekday name, such as Monday
+%B      Month name, such as January
+%m      Month, as a number (01 to 12)
+%d      Day of the month, as a number( 01 to 31)
+%Y      Four-digit year, such as 2015
+%y      Two-digit year, such as 15
+%H      Hour, in 24-hour format(00 to 23)
+%I      Hour, in 12-hour format(01 to 12)
+%p      AM or PM
+%M      Minutes (00 to 59)
+%S      Seconds (00 to 61)
+"""
