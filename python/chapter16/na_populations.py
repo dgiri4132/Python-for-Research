62de8380482a43dda46a55ce745f32ffc8b63686
Darshan Giri <darshangiri785@gmail.com>
2026-08-06 16:49:05 -0500

    Learning how to use maps and tabulating dictionary and mixing stuff up

--- python/chapter16/na_populations.py
+++ python/chapter16/na_populations.py
@@ -1,7 +1,7 @@
 import pygal_maps_world.maps as mp
 
 wm = mp.World()
-wm.title = "Populations of COuntries in North America"
+wm.title = "Populations of Countries in North America"
 wm.add('North America' , {'ca' : 34126000, 'us': 309349000, 'mx' : 113423000})
 
 wm.render_in_browser()
--- python/chapter16/world_population.py
+++ python/chapter16/world_population.py
@@ -1,17 +1,31 @@
 import json
 from country_codes import get_country_code
+import pygal_maps_world.maps as mp
 
 filename = 'python/chapter16/population_data.json'
 with open(filename) as f:
     pop_data = json.load(f)
-
+cc_populations = {}
+cc_pops_1, cc_pops_2, cc_pops_3 = {}, {}, {}
 for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
         country_name = pop_dict['Country Name']
         population = int(float(pop_dict['Value']))
         code = get_country_code(country_name)
         if code:
-            print(country_name + ": " + str(population))
-        else:
-            print('ERROR - ' + country_name)
-            
+            cc_populations[code] = population
+for cc, pop in cc_populations.items():
+    if pop < 10000000:
+        cc_pops_1[cc] = pop
+    elif pop < 1000000000:
+        cc_pops_2[cc] = pop
+    else:
+        cc_pops_3[cc] = pop
+print(len(cc_pops_1), len(cc_pops_2), len(cc_pops_3))
+
+wm = mp.World()
+wm.title = 'World Population in 2010, by Country'
+wm.add('0-10m', cc_pops_1)
+wm.add('10m-1bn', cc_pops_2)
+wm.add('>1bn', cc_pops_3)
+wm.render_in_browser()
