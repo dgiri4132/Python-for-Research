ceb2a2328a5c1af0a5418ceb042dce0c0fa890cc
Darshan Giri <darshangiri785@gmail.com>
2026-08-12 19:25:45 -0500

    Added base to api knowledge like call count or github top repositories

--- python/chapter17/apis.py
+++ python/chapter17/apis.py
@@ -3,6 +3,22 @@ import requests
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 r = requests.get(url)
 print("Status code: ", r.status_code)
-response_dict = r.json()
+response_dict = r.json()#Parses json into dictionary
+print(response_dict.keys())
+print("Total repositories: ", response_dict['total_count'])
 
-print("Total repositorie: ", response_dict['total_count'])
+repo_dicts = response_dict['items']
+print("Repositories returned: ", len(repo_dicts))
+
+for repo_dict in repo_dicts:
+    print('Name: ', repo_dict['name'])
+    print('Owner: ',repo_dict['owner']['login'])
+    print('Stars:', repo_dict['stargazers_count'])
+    print('Repository:', repo_dict['html_url'])
+    print('Created:', repo_dict['created_at'])
+    print('Updated:', repo_dict['updated_at'])
+    print('Description:', repo_dict['description'])
+url1 = 'https://api.github.com/rate_limit'
+r1 = requests.get(url1)
+response_dict1 = r1.json()
+print(response_dict1['resources']['search'])
